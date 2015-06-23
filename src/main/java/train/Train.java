b262c3341a9defe59e235097cb4d2b6f06a6b3cb
Yablokov Aleksey <ya_al@bk.ru>
2015-06-23 19:24:23 +0300

    init commit

---
+++ src/main/java/train/Train.java
@@ -0,0 +1,22 @@
+package train;
+
+/**
+ * Комментарий к классу или интерфейсу.
+ */
+class Train {
+
+    /**
+     * Комментарий к полю (переменной).
+     */
+    int number;
+
+    /**
+     * Комментарий к методу.
+     */
+    Integer getNumber() {
+        return number;
+    }
+
+    TrainType t;
+}
+
---
+++ src/main/java/train/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Комментарий к пакету train.
+ * В этом пакете лежат ценные классы.
+ * todo Создать класс Passenger.
+ *
+ * @author yablokov a.
+ * @author Linus Torvalds
+ * @author Joshua Bloch
+ */
+package train;
